3015591045b10d6212d94106eebdc386d0fc2a34
hieu230201 <hieuop130316@gmail.com>
2022-05-04 23:34:19 +0700

    done error

---
+++ src/main/java/com/example/world_phone/constant/ConstansErrorCode.java
@@ -0,0 +1,9 @@
+package com.example.world_phone.constant;
+
+public interface ConstansErrorCode {
+    String CATEGORY_NAME = "WP-LG-001";
+
+    String CATEGORY_ID = "WP-LG-002";
+
+    String CATEGORY_NAME_SAME = "WP-LG-003";
+}
---
+++ src/main/java/com/example/world_phone/service/ICategoryService.java
@@ -0,0 +1,16 @@
+package com.example.world_phone.service;
+
+import com.example.world_phone.dto.request.category.CategoryReqDto;
+import com.example.world_phone.dto.respone.category.CategoryDTO;
+import com.example.world_phone.dto.respone.category.CategoryResponeDto;
+import com.example.world_phone.entity.CategoryEntity;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<CategoryResponeDto> getAllCategory();
+    CategoryEntity findById(String id);
+    void saveCategory(CategoryReqDto categoryDto);
+    void updateCategory(CategoryDTO categoryDto);
+    void deleteCategory(CategoryEntity categoryEntity);
+}
