21ea7432a60219357dc0e89ddaa09b08396631bd
TienHai2002 <volibear25102501@gmail.com>
2022-10-05 21:40:27 +0700

    push hieu

---
+++ src/main/java/com/example/world_phone/repo/LoaiOsRepo.java
@@ -0,0 +1,11 @@
+package com.example.world_phone.repo;
+
+import com.example.world_phone.entity.LoaiOsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface LoaiOsRepo extends JpaRepository<LoaiOsEntity,Long>{
+    List<LoaiOsEntity> findByDeleteFlagIsFalse();
+}
