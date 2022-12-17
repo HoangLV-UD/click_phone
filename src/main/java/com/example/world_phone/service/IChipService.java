e67112a68a57526e08487c362f578e91d8fc82a7
hadinhphuc <phuchdph17337@fpt.edu.vn>
2022-12-17 21:29:03 +0700

    update

--- src/main/java/com/example/world_phone/service/IChipService.java
+++ src/main/java/com/example/world_phone/service/IChipService.java
@@ -1,19 +1,19 @@
-//package com.example.world_phone.service;
-//
-//import com.example.world_phone.dto.request.attribute.chip.ChipRequestDto;
-//import com.example.world_phone.dto.respone.attribute.chip.ChipRespone;
-//
-//import java.util.List;
-//
-//
-//public interface IChipService {
-//    List<ChipRespone> findAll();
-//
-//    ChipRespone findByCate(String id);
-//
-//    String createChip(ChipRequestDto request);
-//
-//    String updateChip(ChipRequestDto request);
-//
-//    String deleteChip(Long idChip);
-//}
+package com.example.world_phone.service;
+
+import com.example.world_phone.dto.request.attribute.chip.ChipRequestDto;
+import com.example.world_phone.dto.respone.attribute.chip.ChipRespone;
+
+import java.util.List;
+
+
+public interface IChipService {
+    List<ChipRespone> findAll();
+
+    ChipRespone findByCate(String id);
+
+    String createChip(ChipRequestDto request);
+
+    String updateChip(ChipRequestDto request);
+
+    String deleteChip(Long idChip);
+}
