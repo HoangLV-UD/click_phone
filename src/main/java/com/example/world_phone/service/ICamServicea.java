e171be9a018973ffa14543a00006b237c87fece5
tuananhgobom <tuananhgobom@gmail.com>
2022-12-17 22:06:53 +0700

    cam api tuan anh

---
+++ src/main/java/com/example/world_phone/service/ICamServicea.java
@@ -0,0 +1,19 @@
+package com.example.world_phone.service;
+
+import com.example.world_phone.dto.request.attribute.cam.CamRequest;
+import com.example.world_phone.dto.respone.attribute.cam.CamRespone;
+
+import java.util.List;
+
+
+public interface ICamServicea {
+    List<CamRespone> findAll();
+
+    String save(CamRequest request);
+
+    String update(CamRequest request);
+
+    String delete(Long id);
+
+    CamRespone findByID(Long id);
+}
