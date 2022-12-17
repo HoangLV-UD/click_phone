5e6a7a6f3553e59bc5b6947f133baf7e4616f7dd
hadinhphuc <phuchdph17337@fpt.edu.vn>
2022-12-17 21:16:01 +0700

    update

---
+++ src/main/java/com/example/world_phone/repo/ChipPhucRepo.java
@@ -0,0 +1,13 @@
+package com.example.world_phone.repo;
+
+import com.example.world_phone.entity.ChipEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ChipPhucRepo extends JpaRepository<ChipEntity, Long> {
+    List<ChipEntity> findByDeleteFlagIsFalse();
+}
