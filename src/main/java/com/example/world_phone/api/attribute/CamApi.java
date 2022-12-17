d26b271c1c23e93ed5ed7cec0692c9e3d83b88cd
tuananhgobom <tuananhgobom@gmail.com>
2022-12-17 21:57:46 +0700

    cam api tuan anh

--- src/main/java/com/example/world_phone/api/attribute/CamApi.java
+++ src/main/java/com/example/world_phone/api/attribute/CamApi.java
@@ -1,57 +1,49 @@
-//package com.example.world_phone.api.attribute;
-//
-//import com.example.world_phone.dto.request.attribute.cam.CamRequest;
-//import com.example.world_phone.dto.respone.attribute.cam.CamRespone;
-//import com.example.world_phone.service.ICamService;
-//import com.example.world_phone.service.IChipService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * Description:
-// *
-// * @author: hieu
-// * @since: 03/07/2022
-// * Project_name: com.example.world_phone.api.attribute
-// */
-//
-//@RestController
-//@RequestMapping("/api/cam")
-//public class CamApi {
-//
-//    @Autowired
-//    private ICamService service;
-//
-//    @GetMapping("/{id}")
-//    public CamRespone findById(@PathVariable("id") String id) {
-//        return service.findByID(Long.valueOf(id));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<?> addScreen(@RequestBody CamRequest request) {
-//        String status = service.save(request);
-//        if (status.equalsIgnoreCase("ok")) {
-//            return ResponseEntity.ok().body(request);
-//        }
-//        return ResponseEntity.badRequest().body(request);
-//    }
-//
-//    @PutMapping
-//    public ResponseEntity<?> updateScreen(@RequestBody CamRequest request) {
-//        String status = service.update(request);
-//        if (status.equalsIgnoreCase("ok")) {
-//            return ResponseEntity.ok().body(request);
-//        }
-//        return ResponseEntity.badRequest().body(request);
-//    }
-//
-//    @DeleteMapping("{id}")
-//    public ResponseEntity<?> deleteScreen(@PathVariable("id") Long id) {
-//        String status = service.delete(id);
-//        if (status.equalsIgnoreCase("ok")) {
-//            return ResponseEntity.ok().body("ok");
-//        }
-//        return ResponseEntity.badRequest().body("false");
-//    }
-//}
+package com.example.world_phone.api.attribute;
+
+import com.example.world_phone.dto.request.attribute.cam.CamRequest;
+import com.example.world_phone.dto.respone.attribute.cam.CamRespone;
+import com.example.world_phone.service.ICamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/cam")
+public class CamApi {
+
+    @Autowired
+    private ICamService service;
+
+    @GetMapping("/{id}")
+    public CamRespone findById(@PathVariable("id") String id) {
+        return service.findByID(Long.valueOf(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addScreen(@RequestBody CamRequest request) {
+        String status = service.save(request);
+        if (status.equalsIgnoreCase("ok")) {
+            return ResponseEntity.ok().body(request);
+        }
+        return ResponseEntity.badRequest().body(request);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateScreen(@RequestBody CamRequest request) {
+        String status = service.update(request);
+        if (status.equalsIgnoreCase("ok")) {
+            return ResponseEntity.ok().body(request);
+        }
+        return ResponseEntity.badRequest().body(request);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteScreen(@PathVariable("id") Long id) {
+        String status = service.delete(id);
+        if (status.equalsIgnoreCase("ok")) {
+            return ResponseEntity.ok().body("ok");
+        }
+        return ResponseEntity.badRequest().body("false");
+    }
+}
