1e8184809df0c2102217fc7967afc8e8687ed2a5
hadinhphuc <phuchdph17337@fpt.edu.vn>
2022-12-17 21:22:19 +0700

    update

--- src/main/java/com/example/world_phone/service/IPinService.java
+++ src/main/java/com/example/world_phone/service/IPinService.java
@@ -1,19 +1,19 @@
-package com.example.world_phone.service;
-
-import com.example.world_phone.dto.request.attribute.pin.PinRequest;
-import com.example.world_phone.dto.respone.attribute.pin.PinRespone;
-
-import java.util.List;
-
-public interface IPinService {
-
-    List<PinRespone> findAll();
-
-    String addPin(PinRequest request);
-
-    String editPin(PinRequest request);
-
-    String delete(Long id);
-
-    PinRespone findById(Long id);
-}
+//package com.example.world_phone.service;
+//
+//import com.example.world_phone.dto.request.attribute.pin.PinRequest;
+//import com.example.world_phone.dto.respone.attribute.pin.PinRespone;
+//
+//import java.util.List;
+//
+//public interface IPinService {
+//
+//    List<PinRespone> findAll();
+//
+//    String addPin(PinRequest request);
+//
+//    String editPin(PinRequest request);
+//
+//    String delete(Long id);
+//
+//    PinRespone findById(Long id);
+//}
